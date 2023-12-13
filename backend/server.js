970cb9926ba161295f3633ad6f451e8592f8709d
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2023-12-13 22:15:07 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,20 +6,34 @@ app.use(express.json());
 app.use(cors());
 
 // An asynchronous function to simulate an asynchronous task
-const generateMessageAsync = async (data) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(`Async message generated! ${data}`);
-    }, 1000);
-  });
+const generateMessageAsync = async (text, generated_responses, past_user_inputs) => {
+    try {
+        console.log(`Me: ${text}`);
+        const result = await inference.conversational({
+            model: "facebook/blenderbot-1B-distill",
+            parameters: {},
+            inputs: {
+                generated_responses: generated_responses,
+                past_user_inputs: past_user_inputs,
+                text: text
+            }
+        });
+
+        console.log(`Bot: ${result.generated_text}`);
+
+        return result;
+    } catch (error) {
+        console.error("Error:", error.message);
+        return null;
+    }
 };
 
 app.post('/message', async (req, res) => {
   try {
     const message = req.body.message;
 
-    // Simulate an asynchronous task (replace with your actual async logic)
-    const asyncMessage = await generateMessageAsync(2);
+
+    const asyncMessage = await generateMessageAsync(message, [], []);
 
     console.log(`Received message: ${message}`);
     console.log(`Async message: ${asyncMessage}`);
