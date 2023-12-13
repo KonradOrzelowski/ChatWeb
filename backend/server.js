bb9925924b6cbb5c95291b1d6aea480685198041
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2023-12-13 22:32:33 +0100

    Conversation with bot in html

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,50 @@
-import express from 'express';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
+const { HfInference } = require("@huggingface/inference");
+const { readFileSync } = require('fs');
+const path = require('path');
+
+
+const configFilePath = path.join(__dirname, '..', 'config.json');
+
+const rawConfig = readFileSync(configFilePath);
+const config = JSON.parse(rawConfig);
+
+const token = config.token;
+
+const inference = new HfInference(token);
+
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+var generated_responses = [];
+var past_user_inputs = [];
 // An asynchronous function to simulate an asynchronous task
-const generateMessageAsync = async (text, generated_responses, past_user_inputs) => {
-    try {
-        console.log(`Me: ${text}`);
-        const result = await inference.conversational({
-            model: "facebook/blenderbot-1B-distill",
-            parameters: {},
-            inputs: {
-                generated_responses: generated_responses,
-                past_user_inputs: past_user_inputs,
-                text: text
-            }
-        });
+async function runInference(text) {
+    const result = await inference.conversational({
+        model: "facebook/blenderbot-1B-distill",
+        parameters: {},
+        inputs: {
+            generated_responses: generated_responses,
+            past_user_inputs: past_user_inputs,
+            text: text
+        }
+    });
 
-        console.log(`Bot: ${result.generated_text}`);
+    generated_responses = result.conversation.generated_responses.slice(-2);
+    past_user_inputs = result.conversation.past_user_inputs.slice(-2);
 
-        return result;
-    } catch (error) {
-        console.error("Error:", error.message);
-        return null;
-    }
-};
+    return result.generated_text;
+  }
 
 app.post('/message', async (req, res) => {
   try {
     const message = req.body.message;
 
 
-    const asyncMessage = await generateMessageAsync(message, [], []);
+    const asyncMessage = await runInference(message);
 
     console.log(`Received message: ${message}`);
     console.log(`Async message: ${asyncMessage}`);
