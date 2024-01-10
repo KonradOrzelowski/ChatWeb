01b6055a54f2f5372da80394fac6946b8c76fda9
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-01-10 23:01:50 +0100

    Replace JSONs with MongoDB as storage for conversations

--- backend/ConversationsHandler.js
+++ backend/ConversationsHandler.js
@@ -1,42 +1,72 @@
 const fs = require('fs');
 const path = require('path');
 
-function findJsonFiles(folder) {
-    const files = fs.readdirSync(folder);
-    const jsonFiles = [];
+// function findJsonFiles(folder) {
+//     const files = fs.readdirSync(folder);
+//     const jsonFiles = [];
   
-    for (const file of files) {
-      const filePath = path.join(folder, file);
-      const stats = fs.statSync(filePath);
+//     for (const file of files) {
+//       const filePath = path.join(folder, file);
+//       const stats = fs.statSync(filePath);
   
-      if (stats.isDirectory()) {
-        jsonFiles.push(...findJsonFiles(filePath));
-      } else if (path.extname(file).toLowerCase() === '.json') {
-        jsonFiles.push(filePath);
-      }
-    }
+//       if (stats.isDirectory()) {
+//         jsonFiles.push(...findJsonFiles(filePath));
+//       } else if (path.extname(file).toLowerCase() === '.json') {
+//         jsonFiles.push(filePath);
+//       }
+//     }
   
-    return jsonFiles;
-  }
+//     return jsonFiles;
+//   }
 
-function processJsonFiles(jsonFiles) {
-    var index = 0;
-    const list_of_titles = [];
-    const list_of_convs = {};
-    for (const file of jsonFiles) {
-        const rawFile = fs.readFileSync(file);
-        const jsonFile = JSON.parse(rawFile);
+// function processJsonFiles(jsonFiles) {
+//     var index = 0;
+//     const list_of_titles = [];
+//     const list_of_convs = {};
+//     for (const file of jsonFiles) {
+//         const rawFile = fs.readFileSync(file);
+//         const jsonFile = JSON.parse(rawFile);
 
-        list_of_titles.push({'title': jsonFile.title, 'index': index});
+//         list_of_titles.push({'title': jsonFile.title, 'index': index});
         
-        list_of_convs[index] = {'title': jsonFile.title, 'conversation': jsonFile.conversation};
-        index++;
-    }
-    return { list_of_titles, list_of_convs };
+//         list_of_convs[index] = {'title': jsonFile.title, 'conversation': jsonFile.conversation};
+//         index++;
+//     }
+//     return { list_of_titles, list_of_convs };
+// }
+
+// const jsonFiles = findJsonFiles('conversations');
+// const { list_of_titles, list_of_convs } = processJsonFiles(jsonFiles);
+const rawConfig = fs.readFileSync('config.json');
+const config = JSON.parse(rawConfig);
+
+const url = config.url;
+
+const { MongoClient } = require('mongodb');
+
+async function get_all_from_collection(db_name, collection_name){
+    const client = new MongoClient(url);
+    await client.connect();
+    const conversations = await client.db(db_name).collection(collection_name);
+    const array_of_convs = await conversations.find({}).toArray();
+    await client.close()
+
+
+    return(array_of_convs);
+
+    
 }
 
-const jsonFiles = findJsonFiles('conversations');
-const { list_of_titles, list_of_convs } = processJsonFiles(jsonFiles);
+function get_list_of_titles(list_of_convs){
+  var list_of_titles = [];
+  var index = 0;
+  for (const conv of list_of_convs) {
 
-module.exports.list_of_titles = list_of_titles;
-module.exports.list_of_convs = list_of_convs;
+      list_of_titles.push({'title': conv.title, 'index': index});
+      index++;
+  }
+  return list_of_titles;
+};
+
+module.exports.get_all_from_collection = get_all_from_collection;
+module.exports.get_list_of_titles = get_list_of_titles;
--- backend/server.js
+++ backend/server.js
@@ -8,63 +8,59 @@ const cors = require('cors');
  */
 const { runInference } = require('./BotHandler');
 
-/**
- * Importing the list_of_titles and list_of_convs from ConversationsHandler module.
- * @module ConversationsHandler
- * @type {Object}
- * @property {Array} list_of_titles - The list of conversation titles.
- * @property {Array} list_of_convs - The list of conversations.
- */
-const {list_of_titles, list_of_convs} = require('./ConversationsHandler');
 
-// const configFilePath = path.join(__dirname, '..', 'config.json');
+const {get_all_from_collection, get_list_of_titles} = require('./ConversationsHandler');
+
+async function main(){
+
+  const list_of_convs = await get_all_from_collection('ChatWebDB', 'conversations');
+  const list_of_titles = get_list_of_titles(list_of_convs);
 
 
-
-
-
-const app = express();
-app.use(express.json());
-app.use(cors());
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
 
 
 
-app.post('/message', async (req, res) => {
-  try {
-    const message = req.body.message;
+  app.post('/message', async (req, res) => {
+    try {
+      const message = req.body.message;
 
-    const asyncMessage = await runInference(message);
+      const asyncMessage = await runInference(message);
 
-    console.log(`Received message: ${message}`);
-    console.log(`Async message: ${asyncMessage}`);
+      console.log(`Received message: ${message}`);
+      console.log(`Async message: ${asyncMessage}`);
 
-    res.json({ receivedMessage: message, asyncMessage });
-    
-  } catch (error) {
-    console.error('Error processing message:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-
-for(item in list_of_convs){
-  app.get(`/conversations/conv_${item}`, (req, res) => {
-    res.json({ response: list_of_convs[item] });
+      res.json({ receivedMessage: message, asyncMessage });
+      
+    } catch (error) {
+      console.error('Error processing message:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   });
+
+
+  for(item in list_of_convs){
+    app.get(`/conversations/conv_${item}`, (req, res) => {
+      res.json({ response: list_of_convs[item] });
+    });
+  }
+
+  app.get('/conversations/list_of_titles', (req, res) => {
+    res.json({ response: list_of_titles });
+  });
+
+  app.get('/conversations/list_of_convs', (req, res) => {
+    res.json({ response: list_of_convs });
+  });
+
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
 }
 
-app.get('/conversations/list_of_titles', (req, res) => {
-  res.json({ response: list_of_titles });
-});
-
-app.get('/conversations/list_of_convs', (req, res) => {
-  res.json({ response: list_of_convs });
-});
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-
+main();
 
