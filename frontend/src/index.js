c6b7112e8e07ddb26e806a894b260a5748cf1a7c
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-05-15 21:04:09 +0200

    Add webpack

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import './network_requests/new_chat_created.js';
+import './network_requests/fetch_data.js';
+import './network_requests/close_website.js';
+import './network_requests/delete_post.js';
+import './network_requests/save_conversation.js';
+import './network_requests/update_conversation_title.js';
+
+import './utils.js';
+import './custom-alert.js';
+import './send_to_server.js';
+
+import './load_list_of_conversations.js';
+import './script.js';
