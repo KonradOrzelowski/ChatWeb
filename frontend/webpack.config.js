750502986d8a8f2a40bb6438d75d372293b68aae
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-05-28 22:17:22 +0200

    Add dotenv package as a dependency and update webpack.config.js to load environment variables from .env file

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    }
-}
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env)
+    })
+  ]
+};
