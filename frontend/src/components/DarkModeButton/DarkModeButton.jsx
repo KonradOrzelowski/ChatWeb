245ee7598caeb8e14c6a8334bc300fcd0fb60648
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-07-08 19:00:51 +0200

    Refactor DarkModeButton component and file names

--- frontend/src/components/DarkModeButton/DarkModeButton.jsx
+++ frontend/src/components/DarkModeButton/DarkModeButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function darkMode() {
     var element = document.body;
@@ -6,7 +6,36 @@ function darkMode() {
 }
 
 export function DarkModeButton() {
-    return <button onClick={darkMode}>
-        <img id="darkModeIcon" src="assets/icons/dark-mode.svg" alt="Icon description"></img>
-    </button>;
+    // return isDarkMode equal to false and function setIsDarkMode which is use to set value of isDarkMode and trigger a re-render
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    // get document.body and set value of isDarkMode.
+    // It will re render stuff if "dark-mode" is on
+    useEffect(() => {
+        const element = document.body;
+        setIsDarkMode(element.classList.contains("dark-mode"));
+    }, []);
+
+    // This just check 
+    useEffect(() => {
+        const element = document.body;
+        if (isDarkMode) {
+            element.classList.add("dark-mode");
+        } else {
+            element.classList.remove("dark-mode");
+        }
+    }, [isDarkMode]);
+
+    const toggleDarkMode = () => {
+        darkMode();
+        setIsDarkMode(prevMode => !prevMode);
+    };
+
+    const asset = isDarkMode ? "assets/icons/light-mode.svg" : "assets/icons/dark-mode.svg";
+
+    return (
+        <button onClick={toggleDarkMode}>
+            <img id="darkModeIcon" src={asset} alt="Icon description" />
+        </button>
+    );
 }
