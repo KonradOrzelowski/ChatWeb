27461183c51a21b18828955bf55ea328bac10848
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-07-08 20:11:46 +0200

    Add right-side component to index.html

---
+++ frontend/src/components/RightSide/RightSide.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import './RightSide.css';
+import { DarkModeButton } from '../DarkModeButton/DarkModeButton.jsx';
+
+export function RightSide() {
+
+    return (
+        <div>
+            <div className = "save-button-wrapper">
+                <div className = "save-button">Save conversation</div>
+            </div>
+            <DarkModeButton />
+        </div>
+    );
+}
--- frontend/src/loadReactDom.js
+++ frontend/src/loadReactDom.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { DarkModeButton } from './components/DarkModeButton/DarkModeButton.jsx';
-
+// import { DarkModeButton } from './components/DarkModeButton/DarkModeButton.jsx';
+import { RightSide } from './components/RightSide/RightSide.jsx';
 
 export function loadReactDom() {
-    const domNode = document.getElementById('dark-mode-button');
-    ReactDOM.createRoot(domNode).render(<DarkModeButton />);
+    // const domNode = document.getElementById('dark-mode-button');
+    // ReactDOM.createRoot(domNode).render(<DarkModeButton />);
+
+    const rightSideNode = document.getElementById('right-side');
+    ReactDOM.createRoot(rightSideNode).render(<RightSide />);
 }
