0d916ef162e13a7a2aec62e80b3a828fa4caff0c
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-07-20 13:59:41 +0200

    Refactor CSS styles and add right-side component to index.html

--- frontend/src/components/RightSide/RightSide.jsx
+++ frontend/src/components/RightSide/RightSide.jsx
@@ -5,11 +5,12 @@ import { DarkModeButton } from '../DarkModeButton/DarkModeButton.jsx';
 export function RightSide() {
 
     return (
-        <div>
-            <div className = "save-button-wrapper">
-                <div className = "save-button">Save conversation</div>
+        <>
+            <div className="save-button-wrapper">
+                <div className="save-button">Save conversation</div>
+               
             </div>
             <DarkModeButton />
-        </div>
+        </>
     );
 }
