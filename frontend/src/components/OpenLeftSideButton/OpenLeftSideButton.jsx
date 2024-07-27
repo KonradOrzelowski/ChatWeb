091f8fe4c792663092bfb832a1c7ed4db89bb30a
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-07-27 14:34:17 +0200

    Add showListOfConv function to OpenLeftSideButton component

--- frontend/src/components/OpenLeftSideButton/OpenLeftSideButton.jsx
+++ frontend/src/components/OpenLeftSideButton/OpenLeftSideButton.jsx
@@ -3,10 +3,23 @@ import React, { useState, useEffect } from 'react';
 import './OpenLeftSideButton.css';
 
 export function OpenLeftSideButton() {
+    const showListOfConv = () => {
+        
+        var element = document.getElementsByClassName("div-list-of-conversations")[0];
+
+        if (element.style.display === "none") {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
+        }
+
+
+    };
+
     return (
 
         <div>
-            <button>Click me!</button>
+            <button onClick={showListOfConv}>Click me!</button>
         </div>
     )
 }
