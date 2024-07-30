d12ba45aa7d0c12a08261704c24073d305402829
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-07-30 21:34:35 +0200

    Refactor CSS styles and add showListOfConv function to OpenLeftSideButton component

--- frontend/src/components/OpenLeftSideButton/OpenLeftSideButton.jsx
+++ frontend/src/components/OpenLeftSideButton/OpenLeftSideButton.jsx
@@ -3,17 +3,20 @@ import React, { useState, useEffect } from 'react';
 import './OpenLeftSideButton.css';
 
 export function OpenLeftSideButton() {
+    const [isMobileView, setMobileView] = useState(false);
+    
+    useEffect(() => {
+        const width = document.body.scrollWidth;
+        if (width <= 600) {
+            setMobileView(true);
+        }
+    }, []);
+
+
     const showListOfConv = () => {
         
         var element = document.getElementsByClassName("div-list-of-conversations")[0];
-
-        if (element.style.display === "none") {
-            element.style.display = "block";
-        } else {
-            element.style.display = "none";
-        }
-
-
+        
     };
 
     return (
