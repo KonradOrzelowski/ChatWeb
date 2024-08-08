0267ded5cd97a35decc79f6f2ee98a3bbbc0b008
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-08-08 18:21:37 +0200

    Add overlay to OpenLeftSideButton component

--- frontend/src/components/OpenLeftSideButton/OpenLeftSideButton.jsx
+++ frontend/src/components/OpenLeftSideButton/OpenLeftSideButton.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './OpenLeftSideButton.css';
 
 import { fetchData } from '../..//network_requests/fetch_data.js';
 import { getUrl } from '../../get_url.js';
 
 
-function GetChildren({data}) {
+function GetChildren(data) {
     return React.Children.map(data, child => (
         <div className="dropdown-item">
             <p>{child}</p>
@@ -16,7 +16,7 @@ function GetChildren({data}) {
 function GetList({ isOpen, data }){
     if (isOpen) {
         return (
-            <div className="dropdown-menu">
+            <div className="dropdown-menu" >
                 {GetChildren(data)}
             </div>
         )
@@ -27,22 +27,33 @@ function GetList({ isOpen, data }){
 
 export function OpenLeftSideButton() {
     const [open, setOpen] = useState(false);
-    const [data, setData] = useState(null);
 
+    let menuRef = useRef(null);
+    
     useEffect(() => {
-        const getConversation = async () => {
-            const endpointUrl = getUrl('lists/list_of_titles');
-            let conversationTitles = await fetchData(endpointUrl);
-            conversationTitles = conversationTitles.response
-            setData(conversationTitles);
+        const handler = (event) => {
+            if(menuRef.current && !menuRef.current.contains(event.target)){
+                setOpen(false);
+            }
+            
         }
-        getConversation();
-    },[]);
-    console.log(`This is data ${data}`)
-    return (
-        <>
-            <button onClick={() => setOpen(!open)}>Click me!</button>
+        document.addEventListener('mousedown', handler);
+        if(open){
+            document.getElementById('open-left-side').style.width = '20vw';
+            document.getElementsByClassName('test-list')[0].style.width = '20vw';
+            document.getElementsByClassName('overlay')[0].classList.add('active');
+        }else{
+            document.getElementById('open-left-side').style.width = '10vw';
+            document.getElementsByClassName('test-list')[0].style.width = '10vw';
+            document.getElementsByClassName('overlay')[0].classList.remove('active');
+            
+        }
+    });
+
+    var data = ['title1', 'title2', 'title3', 'title4', 'title5', 'title6', 'title7', 'title8', 'title9', 'title10'];
+
+    return (<div className="test-list" ref={menuRef}>
+            <button className='test-button' onClick={() => setOpen(!open)}>Click me!</button>
             <GetList isOpen={open} data={data} />
-        </>
-    );
+        </div>);
 }
