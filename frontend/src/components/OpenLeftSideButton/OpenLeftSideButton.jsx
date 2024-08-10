a30706cfb6647a4eeb20a2125f2cc93e5a575542
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-08-10 14:14:12 +0200

    Refactor OpenLeftSideButton component, add ChildComponent, and update CSS styles

--- frontend/src/components/OpenLeftSideButton/OpenLeftSideButton.jsx
+++ frontend/src/components/OpenLeftSideButton/OpenLeftSideButton.jsx
@@ -5,19 +5,58 @@ import { fetchData } from '../..//network_requests/fetch_data.js';
 import { getUrl } from '../../get_url.js';
 
 
-function GetChildren(data) {
-    return React.Children.map(data, child => (
-        <div className="dropdown-item">
-            <p>{child}</p>
+// function GetChildren(data) {
+//     console.log(`Data in GetChildren: ${data[0].title}`);
+
+//     return React.Children.map(data, (title, _id) => (
+//         <div className="dropdown-item">
+//             <p>{title}</p>
+//         </div>
+//     ))
+// }
+
+const ChildComponent = ({ title  }) => {
+    return (
+      <div>
+        <a>${title}</a>
+        <div class = "bottoms-wrapper">
+            <div class = "bottoms-transtion">
+            </div>
+            <div class = "bottoms">
+                <img id="editIcon" src="assets/icons/edit-pen-icon.svg" alt="Icon description"/>
+                <img id="deleteIcon" src="assets/icons/trash-bin-icon.svg" alt="Icon description"/>
+            </div>
         </div>
-    ))
-}
+      </div>
+
+    );
+  };
 
 function GetList({ isOpen, data }){
+    const [conversationTitles, setConversationTitles] = useState(null);
+
+    useEffect(() => {
+        const _fetchData = async () => {
+            const endpointUrl = getUrl('lists/list_of_titles');
+
+            let listOfTitles = await fetchData(endpointUrl);
+            listOfTitles = listOfTitles.response;
+            
+            console.log(listOfTitles);
+            setConversationTitles(listOfTitles);
+        };
+
+        _fetchData();
+    }, []);
+
+
+
     if (isOpen) {
         return (
             <div className="dropdown-menu" >
-                {GetChildren(data)}
+                {conversationTitles.map((item, index) => (
+                    <ChildComponent key={index} title={item.title}/>
+                ))}
             </div>
         )
     }else{
