66e54671c874be2bf52a4c394877fa1e6fad5cf2
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-08-17 13:02:27 +0200

    Add left side comp

--- frontend/src/components/LeftSide/LeftSide.jsx
+++ frontend/src/components/LeftSide/LeftSide.jsx
@@ -5,13 +5,17 @@ export function LeftSide() {
 
     return (
         <>
-        <p>Test</p>
-            {/* <ul class="list-of-conversations">
-                <li class="conversation-title new-chat">
-                    New chat
-                    <img src="assets/icons/text-document-add-icon.svg" alt="Icon description" style={{maxWidth: '1.25rem', maxHeight: '1.25rem'}} />
-                </li>
-            </ul> */}
+            <div class = "div-list-of-conversations">
+                <ul class = "list-of-conversations">
+                    <li class = "conversation-title new-chat">
+                        New chat
+                        <img src="assets/icons/text-document-add-icon.svg" alt="Icon description" style={{maxWidth: '1.25rem', maxHeight: '1.25rem'}} />
+    
+                    </li>
+                </ul>
+            </div>
+
+            <div id="open-left-side">Test of children</div>
         </>
     );
 }
