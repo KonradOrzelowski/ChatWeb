e479a1f7708a08397cacd43fb7dd74645535cd1d
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-08-20 21:41:42 +0200

    Refactor click event in AddNewChat component and update import paths in script.js

--- frontend/src/components/AddNewChat/AddNewChat.jsx
+++ frontend/src/components/AddNewChat/AddNewChat.jsx
@@ -2,12 +2,20 @@ import React, { useState, useEffect } from 'react';
 
 import './AddNewChat.css';
 
+import { sendNewChatSignal } from '../../network_requests/new_chat_created.js';
+import { clear_conversation } from '../../utils.js';
+
 export function AddNewChat() {
     return (
-
-        <a className="conversation-title new-chat">
+        <div className="conversation-title new-chat" onClick={() =>{
+            sendNewChatSignal();
+            clear_conversation();
+        }}>
             New chat
-        <img src="assets/icons/text-document-add-icon.svg" alt="Icon description" style={{maxWidth: '1.25rem', maxHeight: '1.25rem'}} />
-        </a>
+            <img src="assets/icons/text-document-add-icon.svg"
+                alt="Icon description"
+                style={{maxWidth: '1.25rem', maxHeight: '1.25rem'}}
+            />
+        </div>
     )
 }
