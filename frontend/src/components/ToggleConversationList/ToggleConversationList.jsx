4694d61f63f0edf862caf62a43fc3c4e87788406
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-09-19 21:24:05 +0200

    Update ToggleConversationList component styling and behavior

--- frontend/src/components/ToggleConversationList/ToggleConversationList.jsx
+++ frontend/src/components/ToggleConversationList/ToggleConversationList.jsx
@@ -3,15 +3,31 @@ import './ToggleConversationList.css';
 
 export function ToggleConversationList() {
     return(
-        <div 
-            style={{width: '200px', height: '10vh', backgroundColor: 'blue'}}
-            onClick={ () => {
+    <div>
+        <div id='toggle-blue'
+        style={{width: '200px', height: '10vh', backgroundColor: 'blue'}}
+        onClick={ () => {
 
-                const element = document.getElementsByClassName('div-list-of-conversations')[0];
-                element.classList.toggle('hidden');
-            }}
+            const element = document.getElementsByClassName('div-list-of-conversations')[0];
+            element.classList.toggle('blue-hidden');
+
+        }}
+        >
+        </div>
+        
+        <div id='toggle-red'
+        
+        style={{width: '200px', height: '10vh', backgroundColor: 'red'}}
+        onClick={ () => {
+
+            const element = document.getElementsByClassName('div-list-of-conversations')[0];
+            element.classList.toggle('red-hidden');
+
+        }}
         >
 
         </div>
+    </div>
+
     )
 }
