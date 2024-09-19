9eb4dc62e7ac35bf12c71d8193c8de11dad70d70
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-09-19 22:14:35 +0200

    Fix the naming inside the ToggleConversationList component

--- frontend/src/components/ToggleConversationList/ToggleConversationList.jsx
+++ frontend/src/components/ToggleConversationList/ToggleConversationList.jsx
@@ -4,24 +4,29 @@ import './ToggleConversationList.css';
 export function ToggleConversationList() {
     return(
     <div>
-        <div id='toggle-blue'
+        
+        <div id='toggleWhenSmall'
         style={{width: '200px', height: '10vh', backgroundColor: 'blue'}}
         onClick={ () => {
 
-            const element = document.getElementsByClassName('div-list-of-conversations')[0];
-            element.classList.toggle('blue-hidden');
+            const elementHidden = document.getElementsByClassName('div-list-of-conversations')[0];
+            elementHidden.classList.toggle('when-small-hidden');
+
+            const overlayElement = document.getElementsByClassName('overlay')[0];
+            overlayElement.classList.toggle('active');
+ 
 
         }}
         >
         </div>
         
-        <div id='toggle-red'
+        <div id='toggleWhenLarge'
         
         style={{width: '200px', height: '10vh', backgroundColor: 'red'}}
         onClick={ () => {
 
             const element = document.getElementsByClassName('div-list-of-conversations')[0];
-            element.classList.toggle('red-hidden');
+            element.classList.toggle('when-large-hidden');
 
         }}
         >
