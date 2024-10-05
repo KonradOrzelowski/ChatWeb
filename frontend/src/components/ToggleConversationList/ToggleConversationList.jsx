ba18206ba30649e57c96aab545db702616322c20
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-10-05 20:13:06 +0200

    Refactor ToggleConversationList component to improve code structure and readability

--- frontend/src/components/ToggleConversationList/ToggleConversationList.jsx
+++ frontend/src/components/ToggleConversationList/ToggleConversationList.jsx
@@ -2,42 +2,44 @@ import React, { useState, useEffect } from 'react';
 import './ToggleConversationList.css';
 import { AddNewChat } from '../AddNewChat/AddNewChat.jsx';
 
-onClickToggleWhenSmall = () => {
-    const elementHidden = document.getElementsByClassName('div-list-of-conversations')[0];
-    elementHidden.classList.toggle('when-small-hidden');
-
-    const toogleElement = document.getElementById('toggleWhenSmall');
-    toogleElement.classList.toggle('when-small-hidden');
-
-    const overlayElement = document.getElementsByClassName('overlay')[0];
-    overlayElement.classList.toggle('active');
-}
-
-onClickToggleWhenLarge = () => {
-    const element = document.getElementsByClassName('div-list-of-conversations')[0];
-    element.classList.toggle('when-large-hidden');
-
-    const toogleElement = document.getElementById('toggleWhenLarge');
-    toogleElement.classList.toggle('when-large-hidden');
-
-    const LeftSide = document.getElementById('left-side');
-    LeftSide.classList.toggle('when-large-hidden');
-}
 
 export function ToggleConversationList() {
+
+    const onClickToggleWhenSmall = () => {
+        const elementHidden = document.getElementsByClassName('div-list-of-conversations')[0];
+        elementHidden.classList.toggle('when-small-hidden');
+    
+        const toogleElement = document.getElementById('toggleWhenSmall');
+        toogleElement.classList.toggle('when-small-hidden');
+    
+        const overlayElement = document.getElementsByClassName('overlay')[0];
+        overlayElement.classList.toggle('active');
+    }
+    
+    const onClickToggleWhenLarge = () => {
+        const element = document.getElementsByClassName('div-list-of-conversations')[0];
+        element.classList.toggle('when-large-hidden');
+    
+        const toogleElement = document.getElementById('toggleWhenLarge');
+        toogleElement.classList.toggle('when-large-hidden');
+    
+        const LeftSide = document.getElementById('left-side');
+        LeftSide.classList.toggle('when-large-hidden');
+    }
+
     return(
     <div>
         {/* <AddNewChat/> */}
         <div id='toggleWhenSmall'
         
-        onClick={ onClickToggleWhenSmall() }
+        onClick={ onClickToggleWhenSmall}
         >
         </div>
         
         <div id='toggleWhenLarge'
         
         
-        onClick={ onClickToggleWhenLarge() }
+        onClick={ onClickToggleWhenLarge}
         >
 
         </div>
