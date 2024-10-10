8c103e7cbfc55262598ad1dad7f0e215203c94f0
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-10-10 23:34:19 +0200

    Refactor CSS styles and fix display issues in CloseList and ToggleConversationList components

--- frontend/src/components/CloseList/CloseList.jsx
+++ frontend/src/components/CloseList/CloseList.jsx
@@ -3,23 +3,41 @@ import React, { useState, useEffect } from 'react';
 import './CloseList.css';
 
 export function CloseList() {
+    const onClickToggleWhenSmall = () => {
+        const elementHidden = document.getElementsByClassName('div-list-of-conversations')[0];
+        elementHidden.classList.toggle('when-small-hidden');
+    
+        const toogleElement = document.getElementsByClassName('new-chat')[0];
+        toogleElement.classList.toggle('when-small-hidden');
+    
+        // const overlayElement = document.getElementsByClassName('overlay')[0];
+        // overlayElement.classList.toggle('active');
+    }
+    
+    const onClickToggleWhenLarge = () => {
+        const element = document.getElementsByClassName('div-list-of-conversations')[0];
+        element.classList.toggle('when-large-hidden');
+    
+        const toogleElement = document.getElementsByClassName('new-chat')[0];
+        toogleElement.classList.toggle('when-large-hidden');
+    
+        const LeftSide = document.getElementById('left-side');
+        LeftSide.classList.toggle('when-large-hidden');
+    }
+
+    function onClickToogleList(){
+        
+        const width = window.innerWidth;
+
+        if(width < 600){
+            onClickToggleWhenSmall();
+        }else{
+            onClickToggleWhenLarge();
+        }
+    }
+
     return (
-        <div className="close-list" onClick={() =>{
-
-            const width = window.innerWidth;
-
-            if(width < 600){
-                const toogleElement = document.getElementsByClassName('new-chat')[0];
-                toogleElement.classList.toggle('when-small-hidden');
-                console.log(`toogleElement: ${toogleElement}`);
-            }else{
-                const toogleElement = document.getElementsByClassName('new-chat')[0];
-                toogleElement.classList.toggle('when-large-hidden');
-                console.log(`toogleElement: ${toogleElement}`);
-            }
-
-            
-        }}>
+        <div className="close-list"  onClick={onClickToogleList}>
             <img src="assets/icons/light-mode.svg"
                 alt="Icon description"
             />
--- frontend/src/components/ToggleConversationList/ToggleConversationList.jsx
+++ frontend/src/components/ToggleConversationList/ToggleConversationList.jsx
@@ -5,42 +5,11 @@ import { CloseList } from '../CloseList/CloseList.jsx';
 
 export function ToggleConversationList() {
 
-    const onClickToggleWhenSmall = () => {
-        const elementHidden = document.getElementsByClassName('div-list-of-conversations')[0];
-        elementHidden.classList.toggle('when-small-hidden');
-    
-        const toogleElement = document.getElementById('toogleList');
-        toogleElement.classList.toggle('when-small-hidden');
-    
-        // const overlayElement = document.getElementsByClassName('overlay')[0];
-        // overlayElement.classList.toggle('active');
-    }
-    
-    const onClickToggleWhenLarge = () => {
-        const element = document.getElementsByClassName('div-list-of-conversations')[0];
-        element.classList.toggle('when-large-hidden');
-    
-        const toogleElement = document.getElementById('toogleList');
-        toogleElement.classList.toggle('when-large-hidden');
-    
-        const LeftSide = document.getElementById('left-side');
-        LeftSide.classList.toggle('when-large-hidden');
-    }
 
-    function onClickToogleList(){
-        
-        const width = window.innerWidth;
-
-        if(width < 600){
-            onClickToggleWhenSmall();
-        }else{
-            onClickToggleWhenLarge();
-        }
-    }
 
     return(
     <div>
-        <div id='toogleList' onClick={onClickToogleList}>
+        <div id='toogleList'>
             <CloseList/>
             <AddNewChat/>
         </div>
