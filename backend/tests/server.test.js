fe94d3661088b1af7ed741e00ed1c457d71aad69
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-12-05 20:32:16 +0100

    Fix test GET /lists/list_of_titles

--- backend/tests/server.test.js
+++ backend/tests/server.test.js
@@ -5,12 +5,36 @@ const { getUrl } = require("../get_url");
 
 const request = require("supertest");
 
-describe("GET/", () => {
-    const url = getUrl("");
-    console.log(url);
+// describe("GET/", () => {
+//     const url = getUrl("");
+//     console.log(url);
 
-    it("responds with 200", async () => {
+//     it("responds with 200", async () => {
+//         const response = await request(url).get('');
+//         expect(response.statusCode).toBe(200);
+//     });
+// });
+
+
+
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+  title: { type: String},
+  _id: { type: String },
+  lastChangeDate: { type: String }
+});
+
+const Item = mongoose.model('Item', ItemSchema);
+
+describe("GET /lists/list_of_titles", () => {
+    const url = getUrl("lists/list_of_titles");
+    console.log(url);
+    it("responds with a list of titles", async () => {
         const response = await request(url).get('');
         expect(response.statusCode).toBe(200);
+        expect(response.body.response).toBeDefined(); // Check if 'response' property exists
+
+        const list_of_titles = response.body.response;
     });
 });
