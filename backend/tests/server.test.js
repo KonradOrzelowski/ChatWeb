c7c79b15a83f3ae16242b9205d508d72baccdae0
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-12-05 20:11:25 +0100

    Fix GET/ test

--- backend/tests/server.test.js
+++ backend/tests/server.test.js
@@ -10,22 +10,7 @@ describe("GET/", () => {
     console.log(url);
 
     it("responds with 200", async () => {
-        const response = await request(url);
-
-        console.log(response)
-
-        // expect(response.statusCode).toBe(200);
-        expect(response.text).toBe("Server is running");
+        const response = await request(url).get('');
+        expect(response.statusCode).toBe(200);
     });
 });
-
-
-// describe("GET /lists/list_of_titles", () => {
-//     it("responds with a list of titles", async () => {
-//         const response = await request(`http://${HOST_NAME}:3000/lists/list_of_titles`).get("/");
-//         expect(response.status).toBe(200);
-//         expect(response.body.response).toBeDefined(); // Check if 'response' property exists
-//         expect(Array.isArray(response.body.response)).toBe(true); // Check if 'response' is an array
-//         expect(response.body.response.length).toBeGreaterThan(0); // Check if 'response' array is not empty
-//     }, 120*1000);
-// });
