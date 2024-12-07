34dab7ba34a366a9256e48d6df367df5b2da4bcf
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-12-07 22:56:43 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,14 @@ const http = require("http");
 const cors = require("cors");
 
 const getListsRouting = require("./routes/get_lists_routing");
-const deleteConversationRouting = require("./routes/delete_conversation_routing");
 const conversationsRouting = require("./routes/conversations_routing");
-const incomingMessagesRouting = require("./routes/incoming_messages_routing");
-const saveConversation = require("./routes/save_conversation");
-const updateConversation = require("./routes/update_conversation");
-const refresh = require("./routes/refresh");
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.use(getListsRouting);
-app.use(deleteConversationRouting);
 app.use(conversationsRouting);
-app.use(incomingMessagesRouting);
-app.use(saveConversation);
-app.use(updateConversation);
-app.use(refresh);
 
 var counter = 0;
 
