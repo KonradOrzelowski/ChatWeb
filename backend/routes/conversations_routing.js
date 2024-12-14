24a96f185ac05c8b417195fdd4dbd31e2d428e01
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-12-14 13:12:04 +0100

    Add error handler middleware

--- backend/routes/conversations_routing.js
+++ backend/routes/conversations_routing.js
@@ -3,89 +3,71 @@ const { MongoClient, ObjectId } = require("mongodb");
 const express = require("express");
 const router = express.Router();
 
-const { get_all_from_collection } = require("../ConversationsHandler");
 const { generateResponseFromModel } = require("../generate_response_from_model");
 
 const { MongoDBHandler } = require('../mongoDB-handler');
 
-router.get("/conversations/:id", async (req, res) => {
+const asyncHandler = fn => (req, res, next) => {
+    return Promise.resolve(fn(req, res, next)).catch(next);
+};
 
-    try{
 
-        const mongdbClass = new MongoDBHandler();
-        var list_of_convs = await mongdbClass.get_all_from_collection();
+router.get("/conversations/:id", asyncHandler(async(req, res) => {
+    const mongdbClass = new MongoDBHandler();
+    var list_of_convs = await mongdbClass.get_all_from_collection();
 
-        const { id } = req.params;
-        res.json({ response: list_of_convs[id] });
+    const { id } = req.params;
+    res.json({ response: list_of_convs[id] });
 
-    }catch (error){
-        console.error(`Error while fetchin conversation ${id}: ${error}`);
-    }
+}));
 
-});
+router.delete("/conversations/:id", asyncHandler(async(req, res) => {
 
-router.delete("/conversations/:id", async (req, res) => {
+    const conversationId = req.params.id;
+    console.log(`Deleting conversation with id: ${conversationId}`);
+    
+    const mongdbClass = new MongoDBHandler();
+    await mongdbClass.deletePost(conversationId);
 
-    try{
-        const conversationId = req.params.id;
-        console.log(`Deleting conversation with id: ${conversationId}`);
-        
-        const mongdbClass = new MongoDBHandler();
-        await mongdbClass.deletePost(conversationId);
+    res.json({ response: true });
 
-        res.json({ response: true });
+}));
 
-    }catch (error){
+router.patch("/conversations/:id", asyncHandler(async(req, res) => {
 
-        console.error("Error processing message:", error);
-        res.json({ response: false });
-    }
+    const conversationId = req.params.id;
+    const message = req.body;
+    const newTitle = message.newTitle;
 
-});
+    const mongdbClass = new MongoDBHandler();
+    await mongdbClass.pathConversation(conversationId, newTitle);
 
-router.patch("/conversations/:id", async (req, res) => {
+    res.json({ response: true });
 
-    try{
-        const conversationId = req.params.id;
-        const message = req.body;
-        const newTitle = message.newTitle;
+    console.log("Response sent from update");
 
-        const mongdbClass = new MongoDBHandler();
-        await mongdbClass.pathConversation(conversationId, newTitle);
 
-        res.json({ response: true });
+}));
 
-        console.log("Response sended from update");
-    }catch (error){
-        
-        console.error("Error processing message:", error);
-        res.json({ response: false });
-    }
+router.post("/conversations/:id/messages", asyncHandler(async(req, res) => {
 
-});
 
-router.post("/conversations/:id/messages", async (req, res) => {
+    const conversationId = req.params.id;
 
-    try {
-        const conversationId = req.params.id;
+    const message = req.body.message;
+    const serverResponse = await generateResponseFromModel(message);
 
-        const message = req.body.message;
-        const serverResponse = await generateResponseFromModel(message);
+    let newConversation = [
+        {"speaker" : "You", "message" : message},
+        {"speaker" :" Bot", "message" : serverResponse}
+    ]
 
-        let newConversation = [
-            {"speaker" : "You", "message" : message},
-            {"speaker" :" Bot", "message" : serverResponse}
-        ]
+    const mongdbClass = new MongoDBHandler();
+    await mongdbClass.addConversation(conversationId, newConversation);
 
-        const mongdbClass = new MongoDBHandler();
-        await mongdbClass.addConversation(conversationId, newConversation);
+    res.json({ response:{receivedMessage: message, serverResponse: serverResponse} });
 
-        res.json({ response:{receivedMessage: message, serverResponse: serverResponse} });
 
-    } catch (error) {
-        console.error("Error processing message:", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-});
+}));
 
 module.exports = router;
