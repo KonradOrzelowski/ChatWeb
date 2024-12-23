780f8d97d5f923060105ae92a91eef92e6585f2f
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2024-12-23 16:46:02 +0100

    Rename tests

--- frontend/tests/conversations.test.js
+++ frontend/tests/conversations.test.js
@@ -1,16 +1,6 @@
 const { getDataFromServer } = require("../src/network_requests/getDataFromServer");
 const request = require("supertest");
 
-describe("GET/", () => {
-    const dataFromServer = new getDataFromServer()
-    const id = process.env.CONV_ID;
-
-    it("responds with 200", async () => {
-        const response = await dataFromServer.callEndPoint("GET", `conversations/${id}`);
-        expect(response.status).toBe(200);
-    });
-});
-
 const mongoose = require('mongoose');
 const TitleSchema = {
     date: { type: String, required: true },
